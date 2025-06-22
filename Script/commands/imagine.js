38e117a1edfdf629704764b541f2e34689309083
Rifatcst <rifat240405@gmail.com>
2025-06-22 14:57:37 +0600

    Update imagine.js

--- Script/commands/imagine.js
+++ Script/commands/imagine.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "imagine",
   version: "1.0.",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️",
   description: "generate image from polination",
   commandCategory: "image",
   usages: "query",
