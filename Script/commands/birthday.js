5a44b90f1331b3fd0ac6132600b541bdbf7a4894
Rifatcst <rifat240405@gmail.com>
2025-06-22 13:38:00 +0600

    Update birthday.js

--- Script/commands/birthday.js
+++ Script/commands/birthday.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "bday",
 	version: "1.0.0",
 	hasPermssion: 0,
-    credits: "MAHBUBU SHAON",
+    credits: "RIFAT",
 	description: "See admin's birthday",
     usePrefix: false,
 	commandCategory: "bday",
