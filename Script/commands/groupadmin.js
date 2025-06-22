283399606eaaa17e443d6f9e14afe6edbb11c710
Rifatcst <rifat240405@gmail.com>
2025-06-22 14:51:38 +0600

    Update groupadmin.js

--- Script/commands/groupadmin.js
+++ Script/commands/groupadmin.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "listadmin",
     version: '1.0.0',
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️",
     description: "List of group administrators",
     commandCategory: "Box Chat",
     usages: "dsqtv",
