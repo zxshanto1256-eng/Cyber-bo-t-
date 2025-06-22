263cefe1ef036efe847bceda2c9255bd0c65daa4
Rifatcst <rifat240405@gmail.com>
2025-06-22 14:34:40 +0600

    Update enrile.js

--- Script/commands/enrile.js
+++ Script/commands/enrile.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️",
 	description: "enrile say",
 	commandCategory: "edit-img",
 	usages: "[text]",
