fae728341c5683a23a56547f759fc1f458588e71
Rifatcst <rifat240405@gmail.com>
2025-06-22 14:18:53 +0600

    Update bro.js

--- Script/commands/bro.js
+++ Script/commands/bro.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "brother",
   version: "7.3.1",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
+  credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️", 
   description: "Get Pair From Mention",
   commandCategory: "png",
   usages: "[@mention]",
