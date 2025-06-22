402c252adeb894b9459acc794d4432fc5402dfaf
Rifatcst <rifat240405@gmail.com>
2025-06-22 14:26:34 +0600

    Update clearcache.js

--- Script/commands/clearcache.js
+++ Script/commands/clearcache.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "clearcache",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️",
 	description: "Delete cache file/folder",
 	commandCategory: "system",
 	usages: "",
