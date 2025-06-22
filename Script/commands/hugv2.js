207defc3d678a192ce22192a1c535e6a0c2921fc
Rifatcst <rifat240405@gmail.com>
2025-06-22 14:56:02 +0600

    Update hugv2.js

--- Script/commands/hugv2.js
+++ Script/commands/hugv2.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "hugv2",
     version: "3.1.1",
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️",
     description: "Hug 🥰",
     commandCategory: "img",
     usages: "[@mention]",
