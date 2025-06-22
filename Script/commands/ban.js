d58e6aa8922fa7e37dface21ac4317b247a9689a
Rifatcst <rifat240405@gmail.com>
2025-06-22 13:33:32 +0600

    Update ban.js

--- Script/commands/ban.js
+++ Script/commands/ban.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "ban",
 	version: "2.0.5",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️",
 	description: "Permanently ban members from the group",
 	commandCategory: "group",
 	usages: "[key]",
