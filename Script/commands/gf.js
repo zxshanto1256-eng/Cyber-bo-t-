b61aec988292838a34eb0ebb00050f4a5c34c669
Rifatcst <rifat240405@gmail.com>
2025-06-22 14:49:00 +0600

    Update gf.js

--- Script/commands/gf.js
+++ Script/commands/gf.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "gf",
     version: "7.3.1",
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
+    credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️", 
     description: "Get Pair From Mention",
     commandCategory: "img",
     usages: "[@mention]",
