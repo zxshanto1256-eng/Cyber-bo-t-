a447ae6ffa24877ecc54699bbdd78fd8e15bbb06
Rifatcst <rifat240405@gmail.com>
2025-06-22 14:29:51 +0600

    Update couple.js

--- Script/commands/couple.js
+++ Script/commands/couple.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "couple",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️",
     description: "Seo phi",
     commandCategory: "Love",
     usages: "[tag]",
