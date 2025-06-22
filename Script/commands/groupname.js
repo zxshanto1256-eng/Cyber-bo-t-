b94fd739c1fe7ccdae2f0e3547f312678f29046b
Rifatcst <rifat240405@gmail.com>
2025-06-22 14:52:51 +0600

    Update groupname.js

--- Script/commands/groupname.js
+++ Script/commands/groupname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "groupname",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️",
 	description: "Rename your group",
 	commandCategory: "Box", 
 	usages: "groupname [name]", 
