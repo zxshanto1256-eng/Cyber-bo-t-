08f8158596da5d71d8327f557c99b2658d1ae8b2
Rifatcst <rifat240405@gmail.com>
2025-06-22 14:32:34 +0600

    Update download.js

--- Script/commands/download.js
+++ Script/commands/download.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "download",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "RIFAT ☢️_𖣘 -SIZUKA ⚠️ 𝐁𝐎𝐓_ ☢️",
   description: "Download files",
   commandCategory: "System",
   usages: "download <link> || download <path> <link>",
