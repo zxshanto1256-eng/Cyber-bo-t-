71ed16bbf5b1e10185851f725899cc86ec05ecbb
ARIYAN MALLIK <135170509+ariyan-47@users.noreply.github.com>
2025-07-07 15:48:40 +0600

    Update Bio.js

--- Script/commands/Bio.js
+++ Script/commands/Bio.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "bio",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐀𝐑𝐈𝐘𝐀𝐍 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Change bot's bio",
 	commandCategory: "admin",
 	usages: "bio [text]",
