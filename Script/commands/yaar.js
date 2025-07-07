6ea2198b33b1e6e8a7244a41e8f114933efc51b6
ARIYAN MALLIK <135170509+ariyan-47@users.noreply.github.com>
2025-07-07 15:23:14 +0600

    Update yaar.js 4

--- Script/commands/yaar.js
+++ Script/commands/yaar.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "yaar",
   version: "7.3.1",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
+  credits: "𝐀𝐑𝐈𝐘𝐀𝐍 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
   description: "Get Pair From Mention",
   commandCategory: "png",
   usages: "[@mention]",
