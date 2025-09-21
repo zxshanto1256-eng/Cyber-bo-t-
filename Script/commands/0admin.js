13ee34dbfc7e31df80bdbbe9e0a2b98c39480efd
mdnahidhasannoyon25 <mdnahidhasannoyon25@gmail.com>
2025-09-22 03:48:13 +0600

    Update 0admin.js

--- Script/commands/0admin.js
+++ Script/commands/0admin.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "0admin",
 	version: "1.0.5",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝐍𝐀𝐇𝐈𝐃_ ☢️",
 	description: "Admin Config",
 	commandCategory: "Admin",
 	usages: "Admin",
