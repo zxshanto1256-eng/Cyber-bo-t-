57caa428beb1317e686ee6d8c54a8475338e9e5f
mdnahidhasannoyon25 <mdnahidhasannoyon25@gmail.com>
2025-09-22 04:35:49 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -40,7 +40,7 @@
 ‎        attachment: fs.createReadStream(__dirname + "/cache/1.png")
 ‎    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
 ‎  
-‎    return request(encodeURI(`https://graph.facebook.com/100000478146113/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
+‎    return request(encodeURI(`https://graph.facebook.com/61579913160355/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
 ‎        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
 ‎        .on('close', () => callback());
 ‎};
