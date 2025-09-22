a8f08be9cc79a7f120a0d256c1e68cc7c0e50d3d
mdnahidhasannoyon25 <mdnahidhasannoyon25@gmail.com>
2025-09-22 08:14:47 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -1,24 +1,24 @@
-‎const axios = require("axios");
-‎const request = require("request");
-‎const fs = require("fs-extra");
-‎const moment = require("moment-timezone");
-‎
-‎module.exports.config = {
-‎    name: "admin",
-‎    version: "1.0.0",
-‎    hasPermssion: 0,
-‎    credits: "NAHID", //don't change my credit 
-‎    description: "Show Owner Info",
-‎    commandCategory: "info",
-‎    usages: "",
-‎    cooldowns: 5
-‎};
-‎
-‎module.exports.run = async function({ api, event }) {
-‎    var time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
-‎
-‎    var callback = () => api.sendMessage({
-‎        body: `
+const axios = require("axios");
+const request = require("request");
+const fs = require("fs-extra");
+const moment = require("moment-timezone");
+
+module.exports.config = {
+    name: "admin",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "ULLASH", //don't change my credit 
+    description: "Show Owner Info",
+    commandCategory: "info",
+    usages: "",
+    cooldowns: 5
+};
+
+module.exports.run = async function({ api, event }) {
+    var time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+
+    var callback = () => api.sendMessage({
+        body: `
 ‎┏━━━━━━━━━━━━━━━━━━━━━┓
 ‎┃      🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
 ‎┣━━━━━━━━━━━━━━━━━━━━━┫
@@ -36,11 +36,11 @@
 ‎┣━━━━━━━━━━━━━━━━━━━━━┫
 ‎┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞:  ${time}
 ‎┗━━━━━━━━━━━━━━━━━━━━━┛
-‎        `,
-‎        attachment: fs.createReadStream(__dirname + "/cache/1.png")
-‎    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
-‎  
-‎    return request(encodeURI(`https://graph.facebook.com/61579913160355/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
-‎        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
-‎        .on('close', () => callback());
-‎};
+        `,
+        attachment: fs.createReadStream(__dirname + "/cache/1.png")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
+  
+    return request(encodeURI(`https://graph.facebook.com/61579913160355/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
+        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
+        .on('close', () => callback());
+};
