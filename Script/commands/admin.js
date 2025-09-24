889f1e95be8bdc19199f2ef31d54560990ec4192
shajith-stack <khandakarshahriar95@gmail.com>
2025-09-25 00:29:17 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -3,7 +3,7 @@ const request = require("request");
 const fs = require("fs-extra");
 const moment = require("moment-timezone");
 
-module.exports.config = {
+module.exports.config = 
     name: "admin",
     version: "1.0.0",
     hasPermssion: 0,
@@ -39,7 +39,7 @@ module.exports.run = async function({ api, event }) {
 ┗━━━━━━━━━━━━━━━━━━━━━┛
         `
         attachment: fs.createReadStream(__dirname + "/cache/1.png")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
+      event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
   
     return request(encodeURI(`https://graph.facebook.com/100000478146113/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
         .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
